790edbdb9c962eb0daf7f87b57ca7f78ff1110d4
Dynamicram436 <115802967+Dynamicram436@users.noreply.github.com>
2025-07-14 12:01:38 +0530

    Add files via upload

---
+++ shopzone.js
@@ -0,0 +1,22 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//   document.querySelectorAll(".collapsible-toggle").forEach((toggle) => {
+//     toggle.addEventListener("click", function () {
+//       let menu = this.nextElementSibling; // The submenu <ul>
+//       let arrowIcon = this.querySelector(".arrow");
+
+//       if (menu.offsetHeight !== 0) {
+//         menu.style.maxHeight = "0px";
+//         arrowIcon.classList.remove("rotate-90");
+//       } else {
+//         menu.style.maxHeight = menu.scrollHeight + "px";
+//         arrowIcon.classList.add("rotate-90");
+//       }
+//     });
+//   });
+// });
+
+
+
+
+
+
